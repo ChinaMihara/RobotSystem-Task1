fc3139c8fabcb0d46bd300aeaf81069a9124b13b
China Mihara <72910627+ChinaMihara@users.noreply.github.com>
2020-12-04 19:51:04 +0900

    Update myled.c

--- myled/myled.c
+++ myled/myled.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * Copyright (C) 2020 ChinaMihara + RyuichiUeda. All right reserved.
+ * Copyright (C) 2020 China Mihara + Ryuichi Ueda. All right reserved.
  */
 
 
